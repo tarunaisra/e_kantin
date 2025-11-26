83e389bb1082788a39539ba06a6792588521d177
Rafli <ahmadbachtiar002@gmail.com>
2025-11-26 13:06:59 +0700

    Mengerjakan Widgets

---
+++ lib/widgets/empty_state.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class EmptyStateRapli extends StatelessWidget {
+  final String message;
+  const EmptyStateRapli({required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(message),
+    );
+  }
+}
